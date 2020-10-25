5a5543b0e7a9b56874c486287c6d2bcbe1f2057c
Vitaliy <vetal.morenko@mail.ru>
2020-10-25 19:32:24 +0300

    feat: add decision for tast brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const a = bracketsConfig.map( el => el.join('') )
+
+    for (let i = 0; i < a.length; i++) {
+        if (str.includes(a[i])) str = str.replace(a[i], ''), i = -1
+    }
+    return !str
 }
